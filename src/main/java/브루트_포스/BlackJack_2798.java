0e646d8e06e609a55ee0adc22a351302e27bfb49
seyoungkwon <seyoungkwon29@gmail.com>
2024-10-31 19:02:51 +0900

    #2798 블랙잭

---
+++ src/main/java/브루트_포스/BlackJack_2798.java
@@ -0,0 +1,49 @@
+package 브루트_포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BlackJack_2798 {
+    static int N, M;
+    static int[] cards;
+
+    static int sum() {
+        int answer = 0;
+
+        // 3장 뽑기
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+
+                    int temp = cards[i] + cards[j] + cards[k];
+
+                    if (M == temp)
+                        return temp;
+
+                    if (answer < temp && temp < M)
+                        answer = temp;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        cards = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(sum());
+    }
+}
