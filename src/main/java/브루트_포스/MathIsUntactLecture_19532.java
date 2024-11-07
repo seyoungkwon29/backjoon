23f1e38f8419250f65a40e2613aee675f7ac55aa
seyoungkwon <seyoungkwon29@gmail.com>
2024-11-07 18:14:52 +0900

    #19532 수학은 비대면강의 입니다

---
+++ src/main/java/브루트_포스/MathIsUntactLecture_19532.java
@@ -0,0 +1,31 @@
+package 브루트_포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MathIsUntactLecture_19532 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        for (int i = -999; i <= 999; i++) {
+            for (int j = -999; j <= 999; j++) {
+                if (a * i + b * j == c && d * i + e * j == f) {
+                    sb.append(i).append(" ").append(j);
+                    System.out.println(sb);
+                    return;
+                }
+            }
+        }
+    }
+}
