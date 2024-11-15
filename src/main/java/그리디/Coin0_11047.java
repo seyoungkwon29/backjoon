6ddfc6694bfa863f1de826d24f3b7ac3743154ce
seyoungkwon <seyoungkwon29@gmail.com>
2024-11-15 15:17:00 +0900

    #11047 동전 0

---
+++ src/main/java/그리디/Coin0_11047.java
@@ -0,0 +1,34 @@
+package 그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Coin0_11047 {
+    static int[] coins;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int count = 0;
+
+        coins = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = coins.length - 1; i >= 0; i--) {
+            if (coins[i] > k) continue;
+            else {
+                count += k / coins[i];
+                k %= coins[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/main/java/다이나믹프로그래밍/SugarDelivery_2839.java
@@ -0,0 +1,2 @@
+package 다이나믹프로그래밍;public class SugarDelivery_2839 {
+}
